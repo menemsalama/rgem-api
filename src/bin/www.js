fb83a50bd568441ed485900adab86522196f9e60
menem salama <menem.slama@gmail.com>
2018-03-22 10:16:25 +0200

    use env var port with default

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,5 +1,7 @@
 const app = require('../');
 
-app.listen(3000, () => {
-  console.log(`server is running. 3000`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 })
